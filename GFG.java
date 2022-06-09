cbd318ce916d88c9c686ed96bdae0cc265db025f
merinsamuel <103182657+merinsamuel@users.noreply.github.com>
2022-06-08 22:10:13 -0700

    Add files via upload

--- GFG.java
+++ GFG.java
@@ -1,71 +1,86 @@
+import java.io.*;
 import java.util.*;
-public class GFG
+class stack<T> 
 {
-static void checkSymmetric(int mat[][],int row,int col)
+ArrayList<T>A;
+int top = -1;
+int size;
+stack(int size)
 {
-int i,j,flag=1;
-System.out.print("Matrix formed is");
-for(i=0;i<row;i++)
-{
-for(j=0;j<col;j++)
-{
-System.out.print(mat[i][j]+"\t");
+this.size = size;
+this.A = new ArrayList<T>(size);
 }
-System.out.print("");
+void push(T X)
+{
+if (top + 1 == size) 
+{
+System.out.println("Stack Overflow");
 }
-int[][] transpose=new int[row][col];
-for(i=0;i<row;i++)
+else 
 {
-for(j=0;j<col;j++)
-{
-transpose[j][i]=mat[i][j];
-}
-}
-if(row==col)
-{
-for(i=0;i<row;i++)
-{
-for(j=0;j<col;j++)
-{
-if(mat[i][j]!=transpose[i][j])
-{
-flag=0;
-break;
+top = top + 1;
+if (A.size()>top)
+A.set(top, X);
+else
+A.add(X);
 }
 }
-if(flag==0)
+T top()
 {
-System.out.print("Matrix is not symmetric");
-break;
-}
-}
-if(flag==1)
+if (top == -1) 
 {
-System.out.print("matrix is Symmetric");
-}
+System.out.println("Stack Underflow");
+return null;
 }
 else
+return A.get(top);
+}
+void pop()
 {
-System.out.println("not semmetric");
+if (top == -1) 
+{
+System.out.println("Stack Underflow");
+}
+else
+top--;
+}
+boolean empty() 
+{ 
+return top == -1; 
+}
+public String toString()
+{
+String Ans = "";
+for (int i=0; i<top;i++) 
+{
+Ans += String.valueOf(A.get(i)) + "->";
+}
+Ans += String.valueOf(A.get(top));
+return Ans;
 }
 }
-public static void main(String args[])
+public class GFG 
 {
-Scanner sc=new Scanner(System.in);
-int i,j,row,col,flag=1;
-System.out.print("enter number of row");
-row=sc.nextInt();
-System.out.print("number of col");
-col=sc.nextInt();
-int[][] mat=new int[row][col];
-System.out.print("enter matrix element"); 
-for(i=0;i<row;i++)
+public static void main(String[] args)
 {
-for(j=0;j<col;j++)
-{
-mat[i][j]=sc.nextInt();
-}
-}
-checkSymmetric(mat,row,col);
+stack<Integer>s1=new stack<>(3);
+s1.push(10);
+s1.push(20);
+s1.push(30);
+System.out.println("s1 after pushing 10, 20 and 30 :\n"+ s1);
+s1.pop();
+System.out.println("s1 after pop :\n&"+ s1);
+stack<String>s2 = new stack<>(3);
+s2.push("hello");
+s2.push("world");
+s2.push("java");
+System.out.println("\ns2 after pushing 3 elements :\n"+ s2);
+System.out.println("s2 after pushing 4th element:");
+s2.push("GFG");
+stack<Float>s3 = new stack<>(2);
+s3.push(100.0f);
+s3.push(200.0f);
+System.out.println("\ns3 after pushing 2 elements :\n"+ s3);
+System.out.println("top element of s3:\n"+ s3.top());
 }
 }
